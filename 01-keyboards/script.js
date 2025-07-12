b01c786f39eed5692f39ae7071e11ba680012d74
maysunktong <pattaranan.sunktong@gmail.com>
2025-07-13 01:43:33 +0200

    Small css tweaks

--- 01-keyboards/script.js
+++ 01-keyboards/script.js
@@ -19,6 +19,7 @@ function removePlaying(e) {
   if (e.propertyName !== "transform") return;
   e.target.classList.remove("playing");
 }
+
 const keys = Array.from(document.querySelectorAll(".key"));
 keys.forEach((key) => key.addEventListener("transitionend", removePlaying));
 
