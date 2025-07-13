fdb94c11445fc5210c899ecf7fd83f82cd7be1cc
maysunktong <pattaranan.sunktong@gmail.com>
2025-07-13 14:36:35 +0200

    root

--- 01-keyboards/script.js
+++ 01-keyboards/script.js
@@ -16,12 +16,8 @@ function playSound(e) {
 }
 
 function removePlaying(e) {
-  if (e.propertyName !== "transform") return;
   e.target.classList.remove("playing");
 }
-
-const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) => key.addEventListener("transitionend", removePlaying));
 
 /* Turn machine on/off */
 function startMachine(e) {
@@ -41,7 +37,6 @@ function startMachine(e) {
     startKey.classList.toggle("turnOn");
   } */
 }
-
 
 
 window.addEventListener("keydown", startMachine);
